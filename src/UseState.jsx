a343b1883534cebeabc492f45c41d5b9e6399ea8
Diego Rojas <Diego-27ro@outlook.com>
2022-03-28 23:26:19 -0500

    simple states vs dependant states

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 
+const SECURITY_CODE = "paradigma";
+
 function UseState() {
     
-    const [state,setState] = React.useState(false);
+    const [error,setError] = React.useState(false);
     const [loading,setLoading] = React.useState(true);
+    const [value,setValue] = React.useState("");
+
+    console.log(value)
 
     React.useEffect(()=>{
         //console.log('empieza el efecto')
 
         if(loading){
+            setError(false)
             setTimeout(()=>{
                // console.log('dentro del backend')
-                setLoading(false);
+                
+               setLoading(false);
+
+                if(value !== SECURITY_CODE)
+                    setError(true) 
+                
                // console.log('termina cambio en el backend')
             },2000)
            // console.log('termina el efecto')
@@ -24,10 +35,18 @@ function UseState() {
     <div>
         <h2>Delete Use state</h2>
         <p>please, write the security code</p>
-        {!state && <p>Error: no funcion</p>}
+        {error && <p>Error: no funcion</p>}
         
         {loading && <p>Loading...</p>}
-        <input type="text" placeholder = "codigo de seguridad"value="" />
+        <input 
+        type="text"
+        placeholder = "codigo de seguridad"
+        value={value} 
+        onChange={(e) => {
+            setValue(e.target.value);
+
+        }}
+        />
         <button
         onClick={() => setLoading(prevState => !prevState)}
         > Check!</button>
